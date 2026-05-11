b2c2bf953f5436fc56abffc3b057540eae2bb5be
Ivan Zinovyev <ivan@zinovyev.net>
2026-05-11 23:29:20 +0300

    Fixing notifications

--- common/nvim/.config/nvim/lua/plugins/mini.lua
+++ common/nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    'nvim-mini/mini.notify',
-    version = '*',
-    setup = function()
-      require('mini.notify').setup()
-    end
-  },
+	  {
+	    'nvim-mini/mini.notify',
+	    version = '*',
+	    config = function()
+	      require('mini.notify').setup()
+	    end
+	  },
 }
